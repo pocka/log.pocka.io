9fe88952f627dc5f15b5099953ae2562023f2aa0
pocka <pocka.moe@gmail.com>
2017-08-29 00:23:39 +0900

    Add about page

---
+++ src/js/pages/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section class="hero is-dark">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-2">About</h1>
+          <p class="subtitle is-4">このサイトについて</p>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="container">
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/js/routes.js
+++ src/js/routes.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
 import Top from './pages/Top'
+import About from './pages/About'
 import Posts from './pages/Posts'
 import Post from './pages/Post'
 import NotFound from './pages/NotFound'
@@ -9,6 +10,7 @@ import NotFound from './pages/NotFound'
 export default [
     {path: '*', component: NotFound},
     {path: '/', component: Top},
+    {path: '/about', component: About},
     {path: '/posts', component: Posts},
     {path: '/posts/:name+', component: Post, props: true},
 ]
