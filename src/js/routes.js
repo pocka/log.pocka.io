2b3aefa58732bdee72d162083c9ca8b7e2820e11
pocka <pocka.moe@gmail.com>
2017-08-30 01:23:07 +0900

    タグによる検索ページを追加

--- src/js/routes.js
+++ src/js/routes.js
@@ -5,6 +5,7 @@ import About from './pages/About'
 import Posts from './pages/Posts'
 import Post from './pages/Post'
 import NotFound from './pages/NotFound'
+import Tags from './pages/Tags'
 
 
 export default [
@@ -13,4 +14,5 @@ export default [
     {path: '/about', component: About},
     {path: '/posts', component: Posts},
     {path: '/posts/:name+', component: Post, props: true},
+    {path: '/tags/:tag', component: Tags, props: true},
 ]
