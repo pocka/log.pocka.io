17b1ad83be88a1276889f1ffa462db7d2a21bf86
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-09-13 10:58:38 +0900

    Remove buefy and switch css to bulma's one

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Buefy from 'buefy'
 import VueMeta from 'vue-meta'
 
 import App from './components/App'
@@ -8,7 +7,6 @@ import store from './store'
 import routes from './routes'
 
 
-Vue.use(Buefy)
 Vue.use(VueRouter)
 Vue.use(VueMeta)
 
