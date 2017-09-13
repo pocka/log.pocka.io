13e0e63c1ae8ad1985b9b0c4bf2221937ee444f8
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-09-13 10:50:06 +0900

    src/js/ -> src/

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: `${__dirname}/public`,
