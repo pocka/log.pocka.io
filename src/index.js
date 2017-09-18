91fd3bafb65385b4bfd8681b2673e0adaee4d63f
pocka <pocka.moe@gmail.com>
2017-09-18 23:34:52 +0900

    Uninstall Promise polyfill

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueMeta from 'vue-meta'
-import Promise from 'promise-polyfill'
 
 import App from './components/App'
 import store from './store'
 import routes from './routes'
 
-
-if (!window.Promise) {
-  window.Promise = Promise
-}
 
 Vue.use(VueRouter)
 Vue.use(VueMeta)
