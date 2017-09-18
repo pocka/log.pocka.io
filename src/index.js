b87dd0f676937b33a2bca78d43563be66bd3b981
pocka <pocka.moe@gmail.com>
2017-09-19 08:56:41 +0900

    Revert "Merge pull request #26 from pocka/feature/add-raven"
    This reverts commit 5c23c3213695fecf1573bf96d53cb6ed3ecf72a8, reversing
    changes made to e62de4274331d8c2ff9c0ac1d911787aba20ca78.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueMeta from 'vue-meta'
-import Raven from 'raven-js'
 
 import App from './components/App'
 import store from './store'
 import routes from './routes'
 
-
-Raven.config('https://5382795373e441a8a4791a649ce1b2ab@sentry.io/218959').install()
 
 Vue.use(VueRouter)
 Vue.use(VueMeta)
