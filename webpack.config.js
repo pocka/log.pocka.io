b9f4c9eb100300421c37b284f0ea70e8714ccb58
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-09-19 12:53:12 +0900

    whatwg-fetch -> babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 
 module.exports = {
-  entry: ['whatwg-fetch', './src/index.js'],
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     filename: 'bundle.js',
     path: `${__dirname}/public`,
