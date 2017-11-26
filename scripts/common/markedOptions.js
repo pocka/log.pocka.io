4153a99624514ad80774dab94856b249529ca8b5
pocka <pocka.moe@gmail.com>
2017-11-26 12:09:17 +0900

    markedのオプションを外にだした
    共通化するため

---
+++ scripts/common/markedOptions.js
@@ -0,0 +1,18 @@
+const {highlight, highlightAuto} = require('highlight.js')
+const marked = require('marked')
+
+let mdRenderer = new marked.Renderer()
+mdRenderer.heading = (text, level) => `<h${level + 1}>${text}</h${level + 1}>`
+mdRenderer.link = (href, title, text) => href.match(/^(https?:\/\/|\/\/)/)
+  ? `<a href="${href}" title="${title || ''}" target="_blank">${text}</a>`
+  : `<a href="${href}" title="${title || ''}" data-vue-router-link>${text}</a>`
+
+module.exports = {
+  gfm: true,
+  table: true,
+  breaks: true,
+  highlight(code, lang) {
+    return lang ? highlight(lang, code).value : highlightAuto(code).value
+  },
+  renderer: mdRenderer
+}
--- scripts/md2json.js
+++ scripts/md2json.js
@@ -1,30 +1,12 @@
 const readline = require('readline')
 const marked = require('marked')
 const {loadFront} = require('yaml-front-matter')
-const {highlight, highlightAuto} = require('highlight.js')
 
+const mdOpts = require('./common/markedOptions')
 const getMeta = require('./get-meta')
 
 
 const {stdin, stdout} = process
-
-// Modify markup building process
-let mdRenderer = new marked.Renderer()
-mdRenderer.heading = (text, level) => `<h${level + 1}>${text}</h${level + 1}>`
-mdRenderer.link = (href, title, text) => href.match(/^(https?:\/\/|\/\/)/)
-  ? `<a href="${href}" title="${title || ''}" target="_blank">${text}</a>`
-  : `<a href="${href}" title="${title || ''}" data-vue-router-link>${text}</a>`
-
-// Markdown parsing configuration
-const mdOpts = {
-  gfm: true,
-  table: true,
-  breaks: true,
-  highlight(code, lang) {
-    return lang ? highlight(lang, code).value : highlightAuto(code).value
-  },
-  renderer: mdRenderer,
-}
 
 const rl = readline.createInterface({
   input: stdin,
