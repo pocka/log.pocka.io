249ee0664e8d21d5440404e1111e80bde900d5b3
pocka.moe@gmail.com <pocka.moe@gmail.com>
2017-11-27 13:38:38 +0900

    記事の読み込み状態を表示するようにした

--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -1,5 +1,6 @@
 // Mutations
 const SettingPosts = 'SETTING_POSTS'
+const FetchPosts = 'FETCH_POSTS'
 
 // Actions
 export const LoadPosts = 'LOAD_POSTS'
@@ -9,10 +10,15 @@ export default {
         list: [],
         sort: 'createdAt',
         desc: true,
+        isLoading: false
     }),
     mutations: {
       [SettingPosts](state, {posts}) {
         state.list = posts
+        state.isLoading = false
+      },
+      [FetchPosts](state) {
+        state.isLoading = true
       }
     },
     getters: {
@@ -21,8 +27,14 @@ export default {
       getPostsByTag: (state, getters) => tag => getters.posts.filter(post => !!post.tags.find(t => t === tag)),
     },
     actions: {
-      [LoadPosts]({commit}) {
-        fetch('/posts.json').then(res => {
+      [LoadPosts]({commit, state}) {
+        if (state.list.length > 0) {
+          return Promise.resolve()
+        }
+
+        commit(FetchPosts)
+
+        return fetch('/posts.json').then(res => {
           if (res.status !== 200) {
             return Promise.reject(new Error(`Status code error: ${res.status} ${res.statusText}`))
           }
@@ -36,7 +48,11 @@ export default {
             }))
           })
         }).catch(err => {
-          console.error(err)
+          commit(SettingPosts, {
+            posts: []
+          })
+
+          return Promise.reject(err)
         })
       }
     }
