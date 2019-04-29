bf848fad47b7a72e6cb3ac5079e600ae7e4a4510
pocka <pockawoooh@gmail.com>
2019-04-30 04:26:59 +0900

    perf(blog): Surpress re-rendering

--- packages/blog/src/components/organisms/NavMenu/index.tsx
+++ packages/blog/src/components/organisms/NavMenu/index.tsx
@@ -1,11 +1,9 @@
-/** @jsx createElement */
-import { createElement, ComponentType } from 'react'
-import { withTheme } from 'emotion-theming'
-
-import { useMedia } from 'use-media'
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
+import { Fragment, FC } from 'react'
 
 import { BaseProps } from '~/components/BaseProps'
-import { down } from '~/misc/breakpoints'
+import { down, up } from '~/misc/breakpoints'
 import { Theme } from '~/theme'
 
 import { CompactNavMenu } from './CompactNavMenu'
@@ -13,15 +11,27 @@ import { RegularNavMenu } from './RegularNavMenu'
 
 interface Props extends BaseProps {
   title: string
-  theme: Theme
 }
 
-export const NavMenu = withTheme<ComponentType<Props>>(({ theme, ...rest }) => {
-  const isCompact = useMedia(`(${down(theme.breakpoints.md)})`, true)
-
-  return isCompact ? <CompactNavMenu {...rest} /> : <RegularNavMenu {...rest} />
+export const NavMenu: FC<Props> = (({ ...rest }) => {
+  return <Fragment>
+    <CompactNavMenu css={$compact} {...rest}/>
+    <RegularNavMenu css={$regular} {...rest}/>
+  </Fragment>
 })
 
 export default NavMenu
 
 export { NavMenuItem } from './NavMenuItem'
+
+const $compact = (theme: Theme) => css`
+  @media (${up(theme.breakpoints.md)}) {
+    display: none;
+  }
+`
+
+const $regular = (theme: Theme) => css`
+  @media (${down(theme.breakpoints.md)}) {
+    display: none;
+  }
+`
