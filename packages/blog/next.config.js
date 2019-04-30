1b4e6002de735263e8ef3e3e989d1ae8eab31cdc
pocka <pockawoooh@gmail.com>
2019-04-30 13:34:49 +0900

    chore(blog): Add bundle analytics

--- packages/blog/next.config.js
+++ packages/blog/next.config.js
@@ -1,14 +1,40 @@
 const path = require('path')
 
 const withTs = require('@zeit/next-typescript')
+const withAnalyze = require('@zeit/next-bundle-analyzer')
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withTs({
-  webpack(config, options) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '~': path.resolve(__dirname, './src')
+module.exports = withPlugins(
+  [
+    withTs,
+    [
+      withAnalyze,
+      {
+        analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+        analyzeBrowser: ['browser', 'both'].includes(
+          process.env.BUNDLE_ANALYZE
+        ),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: 'static',
+            reportFilename: '../server.html'
+          },
+          browser: {
+            analyzerMode: 'static',
+            reportFilename: './client.html'
+          }
+        }
+      }
+    ]
+  ],
+  {
+    webpack(config, options) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '~': path.resolve(__dirname, './src')
+      }
+
+      return config
     }
-
-    return config
   }
-})
+)
